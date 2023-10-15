2345206a6919a284ecc6829dbcb0fa46a9457612
Henrique Mendes <henriquemendes13@gmail.com>
2023-10-15 18:10:48 -0300

    Add project files.

---
+++ NotasClasse/Aluno.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotasClasse
+{
+    class Aluno
+    {
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+        public double NotaFinal()
+        {
+            return Nota1 + Nota2 + Nota3;
+        }
+
+        public double Faltou()
+        {
+            return 60-NotaFinal();
+        }
+        public override string ToString()
+        {
+            if (NotaFinal() < 60)
+            {
+                return "REPROVADO. FALTARAM " + Faltou() + " PONTOS";
+               
+            }
+            else
+            {
+                return "APROVADO";
+            }
+        }
+
+
+    }
+}
---
+++ NotasClasse/Program.cs
@@ -0,0 +1,18 @@
+﻿using NotasClasse;
+
+Aluno aluno =  new Aluno();
+
+Console.Write("Nome do aluno: ");
+aluno.Nome = Console.ReadLine();
+
+Console.WriteLine("Digite as três notas do aluno: ");
+aluno.Nota1 = double.Parse(Console.ReadLine());
+aluno.Nota2 = double.Parse(Console.ReadLine());
+aluno.Nota3 = double.Parse(Console.ReadLine());
+
+Console.Write("NOTA FINAL = ");
+Console.WriteLine(aluno.NotaFinal());
+
+Console.WriteLine(aluno);
+
+
